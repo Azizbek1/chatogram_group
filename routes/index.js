ae0a814ec3a97c8143b4a89ce6a7d7886da7c103
Azizbek1 <46714958+Azizbek1@users.noreply.github.com>
2020-08-23 19:51:13 +0500

    Student

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,4 @@ router.get('/', (req, res, next) => {
   res.render('index', { title: 'Welcome To Chatogram Mern Group',});
 });
 
-
-router.get('/chat', (req, res, next) => {
-  res.render('chat', { title: 'Welcome MERN Group' });
-});
-
 module.exports = router;
